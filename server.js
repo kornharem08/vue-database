659edafe0bc435a0425c2c845cf48f23d5acaf85
tanakorn <tana_korn01@hotmail.com>
2018-12-15 14:32:32 +0700

    Finish REST API

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var db = require('./database');
+var cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// index page
+app.get('/', function (req, res) {
+res.send('Express is running');
+});
+
+app.get('/api/supplier/', db.getAllsuppliers);
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+console.log('App is running on http://localhost:' + port);
+});
